a1574f92dcd9d93d93349d96a5131e54480493c9
elishamutang <elishamutang@outlook.com>
2024-12-29 23:41:05 +0800

    [update] Updated styling and updated homepage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from './components/nav/Nav'
+import styles from './App.module.css'
 
 function App() {
   return(
-    <main>
+    <main className={styles.main}>
       <NavBar/>
+      <h1 className={styles.front}>A Streetwear Brand</h1>
     </main>
   )
 }
