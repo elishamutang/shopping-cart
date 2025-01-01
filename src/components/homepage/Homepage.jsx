e4da0ced3cab13aa44eb88e161d595533afacd35
elishamutang <elishamutang@outlook.com>
2025-01-01 19:05:01 +0800

    [update] Setup homepage with fetched items and styled accordingly.

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,32 +1,26 @@
 import styles from './Homepage.module.css'
-import product1 from '../../assets/product1.webp'
 
-const Homepage = () => {
+export default function Homepage({items}) {
+
     return (
         <>
             <section>
-                <h1 className={styles.front}>A STREETWEAR BRAND</h1>
+                <h1 className={styles.front}>ALL IN ONE PLACE</h1>
             </section>
             <section className={styles.store}>
-                    <h2>Shop</h2>
-                    {/* Accept an array of products and display some of it */}
-                    <div className={styles.products}>
-                        {/* Each product must display brand, title, price and image */}
-                        {[1,2,3,4].map((item) => {
-                            return (
-                                <div className={styles.product} key={item}>
-                                    <img src={product1}/>
-                                    <h3>Fucking Awesome</h3>
-                                    <p>Washed Harrington Jacket, Black</p>
-                                    <p>Product {item}</p>
-                                </div>
-                            )
-                        })}
-                    </div>
+                <h2>Shop</h2>
+                <div className={styles.products}>
+                    {items.map((item) => {
+                        return (
+                            <div key={item.id} className={styles.product}>
+                                <img src={item.image} className={styles.productImage}></img>
+                                <h3 className={styles.productTitle}>{item.title}</h3>
+                                <p>${item.price}</p>
+                            </div>
+                        )
+                    })}
+                </div>
             </section>
         </>
-        
     )
 }
-
-export default Homepage
