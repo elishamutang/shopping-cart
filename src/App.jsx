0f8e3ae53ad663e41c49293674e6d72cfbb20b1e
elishamutang <elishamutang@outlook.com>
2025-01-01 19:04:33 +0800

    [update] Fetched items from FakeStore API.'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from 'react'
 import NavBar from './components/nav/Nav'
 import Homepage from './components/homepage/Homepage'
 import styles from './App.module.css'
 
 function App() {
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json)
+      setItems(json)
+    });
+  }, [])
+
   return(
     <main className={styles.main}>
       <NavBar/>
-      <Homepage/>
+      <Homepage items={items}/>
     </main>
   )
 }
