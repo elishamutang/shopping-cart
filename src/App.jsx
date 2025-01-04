c7dd56a0b62e3438c3b80f23ee2238ef15b0df9b
elishamutang <elishamutang@outlook.com>
2025-01-04 12:52:09 +0800

    [update] Added error state and handling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import { useEffect, useState } from 'react'
 import NavBar from './components/nav/Nav'
 import Homepage from './components/homepage/Homepage'
+import Error from './components/error/Error'
 import styles from './App.module.css'
 
 function App() {
 
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
-    .then((res) => res.json())
+    .then((res) => {
+
+      if(res.status >= 400) {
+        throw new Error("server error")
+      }
+
+      return res.json()
+    })
     .then((json) => {
       console.log(json)
       setItems(json)
-    });
+    })
+    .catch((error) => setError(error))
   }, [])
 
   return(
     <main className={styles.main}>
       <NavBar/>
-      <Homepage items={items}/>
+      { error ? <Error/> : <Homepage items={items}/> }
     </main>
   )
 }
---
+++ src/components/error/Error.jsx
@@ -0,0 +1,7 @@
+import styles from './Error.module.css'
+
+export default function Error() {
+    return (
+        <h1 className={styles.error}>A network error was encountered.</h1>
+    )
+}
