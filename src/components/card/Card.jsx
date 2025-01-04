6234bc85696bec0ebb274fe0e23e2b03a958973a
elishamutang <elishamutang@outlook.com>
2025-01-04 18:36:56 +0800

    [update] Added quantity and updated styling.

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,7 +5,10 @@ export default function Card({item}) {
         <div className={styles.product}>
             <img src={item.image} className={styles.productImage}></img>
             <h3 className={styles.productTitle}>{item.title}</h3>
-            <p>${item.price}</p>
+            <p className={styles.pricing}>${item.price}</p>
+            <label>
+                Qty: <input type="number" name="quantity" defaultValue={0}/>
+            </label>
             <button>Add to cart</button>
         </div>
     )
