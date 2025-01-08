2bc6a43d466265ec19a1b07584c56f7a626eea51
elisha <elishamutang@outlook.com>
2025-01-08 22:58:06 +0800

    [update] Ran prettier.

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,28 +1,24 @@
-import { useState } from 'react'
-import Card from '../card/Card'
-import styles from './Homepage.module.css'
+import Card from "../card/Card";
+import styles from "./Homepage.module.css";
 
-export default function Homepage({items, isLoading}) {
-
-    const [quantity, setQuantity] = useState(0);
-
-    return (
-        <>
-            <section>
-                <h1 className={styles.front}>ALL IN ONE PLACE</h1>
-            </section>
-            <section className={styles.store}>
-                <h2>Shop</h2>
-                { isLoading ? <h1>Loading...</h1> : 
-                <div className={styles.products}>
-                    {items.map((item) => {
-                        return (
-                            <Card item={item} key={item.id}/>
-                        )
-                    })}
-                </div>}
-                
-            </section>
-        </>
-    )
+export default function Homepage({ items, isLoading }) {
+  return (
+    <>
+      <section>
+        <h1 className={styles.front}>ALL IN ONE PLACE</h1>
+      </section>
+      <section className={styles.store}>
+        <h2>Shop</h2>
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div className={styles.products}>
+            {items.map((item) => {
+              return <Card item={item} key={item.id} />;
+            })}
+          </div>
+        )}
+      </section>
+    </>
+  );
 }
