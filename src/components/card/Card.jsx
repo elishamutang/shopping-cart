34353d19d7a3c5258cb6cb1dc51689aaf18b6250
elisha <elishamutang@outlook.com>
2025-01-08 22:58:23 +0800

    [update] Added local state for Card component to update quantities.

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,30 @@
-import styles from './Card.module.css'
+import { useState } from "react";
+import styles from "./Card.module.css";
 
-export default function Card({item}) {
-    return (
-        <div className={styles.product}>
-            <img src={item.image} className={styles.productImage}></img>
-            <h3 className={styles.productTitle}>{item.title}</h3>
-            <p className={styles.pricing}>${item.price}</p>
-            <label>
-                Qty: <input type="number" name="quantity" defaultValue={0}/>
-            </label>
-            <button>Add to cart</button>
-        </div>
-    )
+export default function Card({ item }) {
+  const [quantity, setQuantity] = useState(0);
+
+  function handleChange(e) {
+    setQuantity(e.target.value);
+  }
+
+  return (
+    <div className={styles.product}>
+      <img src={item.image} className={styles.productImage}></img>
+      <h3 className={styles.productTitle}>{item.title}</h3>
+      <p className={styles.pricing}>${item.price}</p>
+      <label>
+        Qty:{" "}
+        <input
+          type="number"
+          name="quantity"
+          value={quantity === "" ? 0 : quantity}
+          onChange={handleChange}
+          min={0}
+          max={1000}
+        />
+      </label>
+      <button>Add to cart</button>
+    </div>
+  );
 }
