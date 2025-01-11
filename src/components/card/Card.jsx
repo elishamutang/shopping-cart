b0dfd25d384174afc0dc19fb6ee41dce162b945c
elisha <elishamutang@outlook.com>
2025-01-11 15:37:37 +0800

    [update] Updated Card component to add more types of input validation. Updated styling.

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,11 +3,12 @@ import styles from "./Card.module.css";
 
 export default function Card({ item }) {
   const [quantity, setQuantity] = useState(0);
+  const [buttonHover, setButtonHover] = useState(false);
 
   function handleChange(e) {
     if (e.target.value > 1000) {
       setQuantity(1000);
-    } else if (e.target.value < 0) {
+    } else if (e.target.value < 0 || e.target.value.startsWith("0")) {
       setQuantity("");
     } else {
       setQuantity(e.target.value);
@@ -26,11 +27,29 @@ export default function Card({ item }) {
     }
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (quantity < 1) {
+      alert("Quantity must at least be 1.");
+    } else {
+      console.log(`${quantity} of ${item.title} added to cart.`);
+    }
+  }
+
+  function handleMouseEnter() {
+    setButtonHover(!buttonHover);
+  }
+
+  function handleMouseLeave() {
+    setButtonHover(!buttonHover);
+  }
+
   return (
-    <div className={styles.product}>
+    <form onSubmit={handleSubmit} className={styles.product}>
       <img src={item.image} className={styles.productImage}></img>
       <h3 className={styles.productTitle}>{item.title}</h3>
-      <p className={styles.pricing}>${item.price}</p>
+      <p className={buttonHover ? styles.pricingActive : styles.pricing}>${item.price}</p>
       <label>
         Qty:{" "}
         <input
@@ -42,9 +61,13 @@ export default function Card({ item }) {
           max={1000}
           onBlur={handleBlur}
           onFocus={handleFocus}
+          minLength={1}
+          required
         />
       </label>
-      <button>Add to cart</button>
-    </div>
+      <button type="submit" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        Add to cart
+      </button>
+    </form>
   );
 }
