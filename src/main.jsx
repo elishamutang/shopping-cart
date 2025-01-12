2e434ed3a13024323d8bfcd4d1da96b33802695a
elisha <elishamutang@outlook.com>
2025-01-12 17:17:07 +0800

    [update] Added an ErrorPage for handling bad URLs.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./routes/app/App";
+import ErrorPage from "./routes/errorPage/ErrorPage";
 import "./main.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
