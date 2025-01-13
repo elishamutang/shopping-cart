990df1c115ce32e1fd0fc68e7a3ad7b745e1454b
elisha <elishamutang@outlook.com>
2025-01-13 22:17:03 +0800

    [update] Re-factored App component.

--- src/routes/app/App.jsx
+++ src/routes/app/App.jsx
@@ -1,39 +1,12 @@
-import { useEffect, useState } from "react";
 import NavBar from "../../components/nav/Nav";
-import Homepage from "../../components/homepage/Homepage";
-import Error from "../../components/error/Error";
 import styles from "./App.module.css";
+import { Outlet } from "react-router";
 
-function App() {
-  const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // Consider moving state from Card component to App component.
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => {
-        if (res.status >= 400) {
-          throw new Error("server error");
-        }
-
-        return res.json();
-      })
-      .then((json) => {
-        console.log(json);
-        setItems(json);
-      })
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
-  }, []);
-
+export default function App() {
   return (
     <main className={styles.main}>
       <NavBar />
-      {error ? <Error /> : <Homepage items={items} isLoading={loading} />}
+      <Outlet />
     </main>
   );
 }
-
-export default App;
