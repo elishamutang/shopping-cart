7c915521e76db576d6d4f1582fa4bbb0b58f68e0
elisha <elishamutang@outlook.com>
2025-01-13 22:17:59 +0800

    [update] Re-factored Homepage component and adjusted styling.

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,24 +1,32 @@
+import useFakeData from "../../hooks/FakeData";
 import Card from "../card/Card";
 import styles from "./Homepage.module.css";
+import Error from "../error/Error";
 
-export default function Homepage({ items, isLoading }) {
+export default function Homepage() {
+  const { items, loading, error } = useFakeData();
+
   return (
     <>
-      <section>
-        <h1 className={styles.front}>ALL IN ONE PLACE</h1>
-      </section>
-      <section className={styles.store}>
-        <h2>Shop</h2>
-        {isLoading ? (
-          <h1>Loading...</h1>
-        ) : (
-          <div className={styles.products}>
-            {items.map((item) => {
-              return <Card item={item} key={item.id} />;
-            })}
-          </div>
-        )}
-      </section>
+      {loading ? (
+        <h1 className={styles.loading}>Loading...</h1>
+      ) : error ? (
+        <Error />
+      ) : (
+        <>
+          <section>
+            <h1 className={styles.front}>ALL IN ONE PLACE</h1>
+          </section>
+          <section className={styles.store}>
+            <h2>Shop</h2>
+            <div className={styles.products}>
+              {items.map((item) => {
+                return <Card item={item} key={item.id} />;
+              })}
+            </div>
+          </section>
+        </>
+      )}
     </>
   );
 }
