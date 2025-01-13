409335167b058efe29f6a4ab41077cc423fb8e6c
elisha <elishamutang@outlook.com>
2025-01-13 22:16:35 +0800

    [update] Created custom hook for data fetching.

---
+++ src/hooks/FakeData.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export default function useFakeData() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error("server error");
+        }
+
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setItems(json);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { items, loading, error };
+}
