f186938bee84b0913f802c74827e8ded5f2b39cb
elisha <elishamutang@outlook.com>
2025-01-15 21:35:37 +0800

    [update] Completed Shop component/route.

---
+++ src/routes/shop/Shop.jsx
@@ -0,0 +1,29 @@
+import useFakeData from "../../hooks/FakeData";
+import styles from "./Shop.module.css";
+import Error from "../../components/error/Error";
+import Card from "../../components/card/Card";
+
+export default function Shop() {
+  const { items, loading, error } = useFakeData();
+
+  return (
+    <>
+      <section className={styles.store}>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : error ? (
+          <Error />
+        ) : (
+          <>
+            <h1>This is the shop</h1>
+            <div className={styles.products}>
+              {items.map((item) => {
+                return <Card item={item} key={item.id} />;
+              })}
+            </div>
+          </>
+        )}
+      </section>
+    </>
+  );
+}
