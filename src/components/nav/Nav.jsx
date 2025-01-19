b7627debb67cbee3d3e69b6e53f0a8747e29f303
elisha <elishamutang@outlook.com>
2025-01-19 16:01:33 +0800

    [update] Tally up total number of items in Cart.

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,30 @@
 import { ShoppingBag } from "lucide-react";
+import { Link } from "react-router";
 import styles from "./Nav.module.css";
+import { useContext } from "react";
+import { CartContext } from "../../routes/app/App";
 
 const NavBar = () => {
+  const { cart } = useContext(CartContext);
+
+  // Tally up total number of items in Cart.
+  let total = 0;
+  if (cart.length > 0) {
+    total = cart.reduce((accumulator, currVal) => {
+      return accumulator + currVal.amount;
+    }, 0);
+  }
+
   return (
     <nav className={styles.nav}>
-      {/* Make as link tag from React Router */}
       <ul className={styles.ul}>
         <li>
-          <h1>All In One Place</h1>
+          <Link to={"/"} className={styles.home}>
+            <h1>All In One Place</h1>
+          </Link>
         </li>
         <li>
-          <a href="#">Shop</a>
+          <Link to={"shop"}>Shop</Link>
         </li>
       </ul>
       <ul>
@@ -21,7 +35,7 @@ const NavBar = () => {
         <li>
           <a href="#">
             Cart
-            <ShoppingBag className={styles.shoppingBagIcon} />
+            <ShoppingBag className={styles.shoppingBagIcon} />({total})
           </a>
         </li>
       </ul>
