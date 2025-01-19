f007b15a71364bbbc699f9489a8d395df958dea8
elisha <elishamutang@outlook.com>
2025-01-19 16:01:10 +0800

    [update] Utilized useContext to pass state and state updater function to App's children.

--- src/routes/app/App.jsx
+++ src/routes/app/App.jsx
@@ -1,12 +1,19 @@
+import { useState, createContext } from "react";
 import NavBar from "../../components/nav/Nav";
 import styles from "./App.module.css";
 import { Outlet } from "react-router";
 
+export const CartContext = createContext(0);
+
 export default function App() {
+  const [cart, setCart] = useState([]);
+
   return (
     <main className={styles.main}>
-      <NavBar />
-      <Outlet />
+      <CartContext.Provider value={{ cart, setCart }}>
+        <NavBar />
+        <Outlet />
+      </CartContext.Provider>
     </main>
   );
 }
