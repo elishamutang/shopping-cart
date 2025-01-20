53d231aa9c8e0d643d18c3216062eb8f1f9bd0af
elisha <elishamutang@outlook.com>
2025-01-20 23:20:01 +0800

    [update] Most of website functionality being controlled from App component. Added toggle for Cart.

--- src/routes/app/App.jsx
+++ src/routes/app/App.jsx
@@ -2,18 +2,65 @@ import { useState, createContext } from "react";
 import NavBar from "../../components/nav/Nav";
 import styles from "./App.module.css";
 import { Outlet } from "react-router";
+import { X } from "lucide-react";
 
 export const CartContext = createContext(0);
 
 export default function App() {
   const [cart, setCart] = useState([]);
+  const [isCartOpen, setCartOpen] = useState(false);
+
+  // Tally up total number of items in Cart.
+  let total = 0;
+  if (cart.length > 0) {
+    total = cart.reduce((accumulator, currVal) => {
+      return accumulator + currVal.amount;
+    }, 0);
+  }
+
+  // Show Cart
+  function openCart() {
+    setCartOpen(!isCartOpen);
+    console.log("cart open");
+  }
+
+  // Close Cart
+  function closeCart() {
+    setCartOpen(!isCartOpen);
+    console.log("cart closed");
+  }
+
+  console.log(cart);
 
   return (
     <main className={styles.main}>
       <CartContext.Provider value={{ cart, setCart }}>
-        <NavBar />
+        <NavBar openCart={openCart} total={total} />
         <Outlet />
       </CartContext.Provider>
+      {isCartOpen && (
+        <div className={styles.sidebar}>
+          <div className={styles.top}>
+            <h3>Cart ({total})</h3>
+            <X onClick={closeCart} className={styles.closeCart} />
+          </div>
+          <div className={styles.productsCart}>
+            {total > 0 &&
+              cart.map((cartItem) => {
+                return (
+                  <div key={cartItem.id} className={styles.products}>
+                    <img className={styles.productIcon} src={cartItem.icon}></img>
+                    <p className={styles.productTitle}>{cartItem.product}</p>
+                    <p className={styles.productQty}>
+                      <span>Qty:</span> {cartItem.amount}
+                    </p>
+                    <p>${cartItem.price * cartItem.amount}</p>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      )}
     </main>
   );
 }
