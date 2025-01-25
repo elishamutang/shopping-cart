6f17003f6b98626c48a8c9302d8fad15029d1196
elisha <elishamutang@outlook.com>
2025-01-25 17:12:13 +0800

    [update] Added Wishlist.

---
+++ src/routes/wishlist/Wishlist.jsx
@@ -0,0 +1,9 @@
+import styles from "../../main.module.css";
+
+export default function Wishlist() {
+  return (
+    <section className={styles.store}>
+      <h1>This is your wishlist.</h1>
+    </section>
+  );
+}
