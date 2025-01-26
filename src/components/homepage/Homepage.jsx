6904513555e1cfd62841de9b7035e742bb0b26c8
elisha <elishamutang@outlook.com>
2025-01-26 21:12:53 +0800

    [update] Added wishlist array. Ensure items in wishlist array are displayed appropriately in Homepage component.

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,38 +1,37 @@
-import useFakeData from "../../hooks/FakeData";
 import Card from "../card/Card";
 import styles from "./Homepage.module.css";
-import Error from "../error/Error";
 import { Link } from "react-router";
+import { useContext } from "react";
+import { DataContext, WishlistContext } from "../../routes/app/App";
 
 export default function Homepage() {
-  const { items, loading, error } = useFakeData();
+  const { items } = useContext(DataContext);
+  const { wishlist } = useContext(WishlistContext);
 
   return (
     <>
-      {loading ? (
-        <h1 className={styles.loading}>Loading...</h1>
-      ) : error ? (
-        <Error />
-      ) : (
-        <>
-          <section>
-            <h1 className={styles.front}>ALL IN ONE PLACE</h1>
-          </section>
-          <section className={styles.store}>
-            <h1>Shop</h1>
-            <div className={styles.products}>
-              {items.map((item, idx) => {
-                if (idx < 3) {
-                  return <Card item={item} key={item.id} />;
-                }
-              })}
-              <Link to={"shop"} className={styles.seeMoreBtn}>
-                See more
-              </Link>
-            </div>
-          </section>
-        </>
-      )}
+      <section>
+        <h1 className={styles.front}>ALL IN ONE PLACE</h1>
+      </section>
+      <section className={styles.store}>
+        <h1>Shop</h1>
+        <div className={styles.products}>
+          {items.map((item, idx) => {
+            if (idx < 3) {
+              const wishlistItem = wishlist.find((wishlistItem) => wishlistItem.id === item.id);
+
+              if (wishlistItem) {
+                return <Card key={item.id} item={wishlistItem} />;
+              } else {
+                return <Card key={item.id} item={item} />;
+              }
+            }
+          })}
+          <Link to={"shop"} className={styles.seeMoreBtn}>
+            See more
+          </Link>
+        </div>
+      </section>
     </>
   );
 }
