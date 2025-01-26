bdfdd0e7504724a6a60bce317e8557ce44c1d355
elisha <elishamutang@outlook.com>
2025-01-26 21:25:17 +0800

    [update] Completed Wishlist page.

--- src/routes/wishlist/Wishlist.jsx
+++ src/routes/wishlist/Wishlist.jsx
@@ -1,9 +1,25 @@
+import { useContext } from "react";
 import styles from "../../main.module.css";
+import { WishlistContext } from "../app/App";
+import Card from "../../components/card/Card";
 
 export default function Wishlist() {
+  const { wishlist } = useContext(WishlistContext);
+
   return (
     <section className={styles.store}>
-      <h1>This is your wishlist.</h1>
+      {wishlist.length === 0 ? (
+        <h1>Your wishlist is empty.</h1>
+      ) : (
+        <>
+          <h1>This is your wishlist.</h1>
+          <div className={styles.products}>
+            {wishlist.map((item) => {
+              return <Card key={item.id} item={item} />;
+            })}
+          </div>
+        </>
+      )}
     </section>
   );
 }
