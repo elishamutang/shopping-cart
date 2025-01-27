e1e6fa6b07fc108f05dd86a0dd065cc15d91bf3e
elisha <elishamutang@outlook.com>
2025-01-27 21:24:21 +0800

    [update] Created mocked fetch for use in tests.

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,6 +1,11 @@
-import { expect, afterEach } from "vitest";
+import { expect, afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import createFetchMock from "vitest-fetch-mock";
+
+const fetchMocker = createFetchMock(vi);
+
+fetchMocker.enableMocks();
 
 expect.extend(matchers);
 
