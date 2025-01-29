6ccf4e21ae9b653d7f001df0b73ab56e1f263916
elisha <elishamutang@outlook.com>
2025-01-29 23:19:13 +0800

    [update] Updated file.

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,11 +1,6 @@
-import { expect, afterEach, vi } from "vitest";
+import { expect, afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
-import createFetchMock from "vitest-fetch-mock";
-
-const fetchMocker = createFetchMock(vi);
-
-fetchMocker.enableMocks();
 
 expect.extend(matchers);
 
