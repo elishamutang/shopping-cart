ce96d32c893c5b6aba1af49ff8d560fd7343096c
elisha <elishamutang@outlook.com>
2025-02-04 21:43:07 +0800

    [update] Updated App tests.

--- src/routes/app/App.test.jsx
+++ src/routes/app/App.test.jsx
@@ -1,20 +1,12 @@
-import { describe, it, expect, afterEach } from "vitest";
-import { render, screen, act, cleanup, waitFor } from "@testing-library/react";
-import App, { DataContext, WishlistContext } from "./App";
-import Homepage from "../../components/homepage/Homepage";
+import { describe, it, expect, beforeEach, vi, afterEach } from "vitest";
+import { render, screen, act, renderHook, waitFor, cleanup } from "@testing-library/react";
+import useFakeData from "../../hooks/FakeData";
+import App from "./App";
 
 afterEach(cleanup);
 
-const mockResponse = [
-  { id: 1, title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops" },
-  { id: 2, title: "Mens Casual Premium Slim Fit T-Shirts " },
-  { id: 3, title: "Mens Cotton Jacket" },
-];
-
-// Update code to utilise MSW (Mock Service Worker), refer to React Testing Library example.
-
 describe("Shopping cart", () => {
-  it("Load App sidebar", async () => {
+  it("sidebar component exists", async () => {
     await act(async () => {
       render(<App />);
     });
@@ -24,19 +16,41 @@ describe("Shopping cart", () => {
     expect(sidebar).toBeInTheDocument();
   });
 
-  it("Data is loaded in Homepage component", async () => {
-    await act(async () => {
-      <DataContext.Provider value={{ mockResponse }}>
-        <WishlistContext.Provider value={{}}>
-          <Homepage />
-        </WishlistContext.Provider>
-      </DataContext.Provider>;
+  it("should return the initial values for items, loading and error ", async () => {
+    const { result } = renderHook(() => useFakeData());
+    console.log(result.current);
+
+    expect(result.current.items).toEqual([]);
+    expect(result.current.loading).toEqual(true);
+    expect(result.current.error).toBe(null);
+  });
+
+  describe("when data is fetched succesfully", () => {
+    let mockedData;
+    let response;
+
+    beforeEach(() => {
+      mockedData = {
+        items: [
+          { id: 1, title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops" },
+          { id: 2, title: "Mens Casual Premium Slim Fit T-Shirts " },
+          { id: 3, title: "Mens Cotton Jacket" },
+        ],
+        loading: false,
+        error: null,
+      };
+
+      response = { json: vi.fn().mockResolvedValue(mockedData) };
+
+      global.fetch = vi.fn().mockResolvedValue(response);
     });
 
-    const testElem = await screen.findByTestId("productsDiv");
+    it("it should return data", async () => {
+      const { result } = renderHook(() => useFakeData());
 
-    await waitFor(() => {
-      expect(testElem).toBeInTheDocument();
+      await waitFor(() => {
+        expect(result.current.items).toEqual(mockedData);
+      });
     });
   });
 });
